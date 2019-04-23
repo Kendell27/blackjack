5867a36da1e423601dfed483f48cf28a9717307c
Kendell Bonilla Toruño <kendellb.t@hotmail.com>
2019-04-23 11:46:08 -0600

    Cambiando Palo de carta

--- blackjack/blackjack/carta.cpp
+++ blackjack/blackjack/carta.cpp
@@ -1,18 +1,18 @@
 // carta.cpp
-// DefiniciÛn de la clase Carta
+// Definici√≥n de la clase Carta
 
 #include "carta.h"
 
 // Constructor por Default
-// Crea un AS de ESPADAS y boca abajo
-Carta::Carta() : valorCarta(Rango::AS), paloCarta(Palo::ESPADAS), bocaArriba(false) { }
+// Crea un AS de TREBOL y boca abajo
+Carta::Carta() : valorCarta(Rango::AS), paloCarta(Palo::TREBOL), bocaArriba(false) { }
 
-// Constructor por par·metros
-// Recibe el rango, palo y la posiciÛn inicial de la carta
+// Constructor por par√°metros
+// Recibe el rango, palo y la posici√≥n inicial de la carta
 Carta::Carta(Rango r, Palo p, bool b) : valorCarta(r), paloCarta(p), bocaArriba(b) { }
 
 // Destructor
-// VacÌo puesto que no hay memoria por liberar en Carta
+// Vac√≠o puesto que no hay memoria por liberar en Carta
 Carta::~Carta() { }
 
 // Actualiza el valor de la carta - Rango 
